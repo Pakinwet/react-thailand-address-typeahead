93bb36ec175cc3c6123746758b2f559291ea5d38
Pakinwet Saksamerprom <pakinwet@gmail.com>
2020-06-04 06:23:17 +0000

    Delete AddressTypeahead.component.spec.js

--- __test__/AddressTypeahead.component.spec.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var AddressTypeahead = require('../AddressTypeahead.component');
-
-describe('AddressTypeahead component test', function () {
-  it('should render Input box correctly');
-});
